a4d12904e74ac439240db41962b5b307ee603b38
summer <948605548@qq.com>
2019-06-18 18:26:38 +0800

    添加超级管理员角色&添加管理员账号&加入静态文件

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from app import db
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model):
@@ -13,7 +14,7 @@ class User(db.Model):
     phone = db.Column(db.String(11), unique=True)  # 手机号
     info = db.Column(db.Text)  # 个性化介绍
     face = db.Column(db.String(255), unique=True)  # 头像
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 注册时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 注册时间
     uuid = db.Column(db.String(255), unique=True)  # 唯一标识符
     comments = db.relationship("Comment", backref="user")  # 评论外键关系关联
     userlog = db.relationship("Userlog", backref="user")  # 会员日志外键关系关联
@@ -29,7 +30,7 @@ class Userlog(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))  # 所属会员
     ip = db.Column(db.String(100))  # 登录ip
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 登录时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 登录时间
 
     def __repr__(self):
         return "<userlog: %r>" % self.id
@@ -40,7 +41,7 @@ class Tag(db.Model):
     __tablename__ = "tag"
     id = db.Column(db.Integer, primary_key=True)  # 编号
     name = db.Column(db.String(100), unique=True)  # 标题
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
     movies = db.relationship("Movie", backref='tag')  # 电影外键关系关联
 
     def __repr__(self):
@@ -62,7 +63,7 @@ class Movie(db.Model):
     area = db.Column(db.String(255))  # 上映地区
     release_time = db.Column(db.Date)  # 上映时间
     length = db.Column(db.String(100))  # 播放时间
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
     comments = db.relationship("Comment", backref="movie")  # 评论外键关系关联
     miviecols = db.relationship("Moviecol", backref="movie")  # 收藏外键关系关联
 
@@ -76,7 +77,7 @@ class Preview(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     title = db.Column(db.String(255), unique=True)  # 标题
     logo = db.Column(db.String(255), unique=True)  # 封面
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow())  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now())  # 添加时间
 
     def __repr__(self):
         return "<Preview: %r>" % self.title
@@ -89,7 +90,7 @@ class Comment(db.Model):
     content = db.Column(db.Text)  # 内容
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))  # 所属电影
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
 
     def __repr__(self):
         return "<Comment: %r>" % self.id
@@ -101,7 +102,7 @@ class Moviecol(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))  # 所属电影
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
 
     def __repr__(self):
         return "<Moviecol: %r>" % self.id
@@ -113,7 +114,7 @@ class Auth(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     name = db.Column(db.String(100), unique=True)  # 名称
     url = db.Column(db.String(255), unique=True)  # 地址
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
 
     def __repr__(self):
         return "<Auth: %r>" % self.name
@@ -125,7 +126,7 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     name = db.Column(db.String(100), unique=True)  # 名称
     auths = db.Column(db.String(600))  # 地址
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)  # 添加时间
     admins = db.relationship("Admin", backref="role")  # 管理员外键关系关联
 
     def __repr__(self):
@@ -140,7 +141,7 @@ class Admin(db.Model):
     pwd = db.Column(db.String(100))  # 管理员密码
     is_super = db.Column(db.SmallInteger)  # 是否为超级管理员，0为超级管理员
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))  # 所属角色
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
     adminlogs = db.relationship("AdminLog", backref="admin")  # 管理员日志外键关系关联
     oplogs = db.relationship("OpLog", backref="admin")  # 管理员操作日志外键关系关联
 
@@ -154,7 +155,7 @@ class AdminLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 编号
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))  # 所属会员
     ip = db.Column(db.String(100))  # 登录IP
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<AdminLog: %r>" % self.id
@@ -167,7 +168,24 @@ class OpLog(db.Model):
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))  # 所属管理
     ip = db.Column(db.String(100))  # 登录IP
     reason = db.Column(db.String(600))  # 操作原因
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<OpLog: %r>" % self.id
+
+
+if __name__ == "__main__":
+    # role=Role(
+    #     name="超级管理员",
+    #     auths=""
+    # )
+    # db.session.add(role)
+    # db.session.commit()
+    admin = Admin(
+        name="summer",
+        pwd=generate_password_hash("111111"),
+        is_super=0,  # 超级管理员
+        role_id=2
+    )
+    db.session.add(admin)
+    db.session.commit()
